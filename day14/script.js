0466bc45249973696798eaa9c44714d9aa175afc
andy1729 <ujjawalanand1729@gmail.com>
2015-10-18 21:00:09 +0530

    started day14

---
+++ day14/script.js
@@ -0,0 +1,53 @@
+ function getSpentTime(time) {
+   var t = Date.parse(new Date()) - Date.parse(time);
+   var seconds = Math.floor((t/1000) % 60);
+   var minutes = Math.floor((t/(1000*60))%60);
+   var hours = Math.floor((t/(1000*60*60))%24);
+   var days = Math.floor(t/(1000*60*60*24));
+   
+   return {
+     'total' : t,
+     'days' : days,
+     'hours' : hours,
+     'minutes' : minutes,
+     'seconds' : seconds
+   };
+ }
+
+function initializeClock(id,time) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+  var timeInterval = setInterval(function(){
+    var t = getSpentTime(time);
+    daysSpan.innerHTML = (t.days);
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+    if(t.total <=0) {
+      clearInterval(timeInterval);
+    }
+  },1000);
+}
+function currentTime() {
+  var t = Date.parse(new Date());
+  
+  return {
+    'hours' : Math.floor((t/(1000*60*60))%24),
+    'minutes' : Math.floor((t/(1000*60))%60),
+    'seconds' : Math.floor((t/1000)%60)
+  };
+}
+function getCurrentTime(id) {
+ clock = document.getElementById(id);
+  var updateCurrentTime = setInterval(function() {
+    var t = currentTime();
+    clock.innerHTML = 'current time (UTC): ' + t.hours +' : ' + t.minutes + ' : ' + t.seconds;
+},1000);
+}
+
+var deadline = 'October 03 2015 00:00:50 UTC+0500';
+initializeClock('clockdiv', deadline);
+getCurrentTime('currentTime');
